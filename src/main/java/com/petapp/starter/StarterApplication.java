b135c91a0aa3ed21ea3c23e43a5709ceec3870ba
Ivan Graciarena <ivan.graciarena@globallogic.com>
2023-08-12 15:20:06 -0300

    adding author doc to project

--- src/main/java/com/petapp/starter/StarterApplication.java
+++ src/main/java/com/petapp/starter/StarterApplication.java
@@ -3,6 +3,11 @@ package com.petapp.starter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author ivan.graciarena
+ * @created 12/08/2023 - 15:18
+ * @project starter
+ */
 @SpringBootApplication
 public class StarterApplication {
 
