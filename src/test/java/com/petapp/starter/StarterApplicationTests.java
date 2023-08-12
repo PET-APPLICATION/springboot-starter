447cda852dc917f4c2ba809642f58a6bf9a18218
Ivan Graciarena <ivan.graciarena@globallogic.com>
2023-08-12 14:49:00 -0300

    Adding starter module

---
+++ src/test/java/com/petapp/starter/StarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petapp.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
